4ac4b62d651b78ff908e4ec610d07dcfcf822e62
ErickFCL <erickllico1@gmail.com>
2020-10-16 18:26:03 -0500

    Agregar archivos de proyecto.

---
+++ CRUD/Controllers/AlumnoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CRUD.Controllers
+{
+    public class AlumnoController : Controller
+    {
+       public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ CRUD/Models/Alumno.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUD.Models
+{
+    public class Alumno
+    {
+        public int Id { get; set; }
+        public String Nombre { get; set; }
+        public String Curso { get; set; }
+    }
+}
